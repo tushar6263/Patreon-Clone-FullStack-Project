15fcf7dd07163599e7fce7c4b117eb6ef15d2e0d
tushar626346 <tushar626346@gmail.com>
2025-01-02 22:53:15 +0530

    add UI on the login page

---
+++ components/SessionWrapper.js
@@ -0,0 +1,10 @@
+import { SessionProvider } from "next-auth/react"
+
+export default function SessionWrapper({children}){
+ 
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  )
+}
