cc593970f0dcb489c0bfe9e53d85b74f88832132
tushar626346 <tushar626346@gmail.com>
2025-01-02 23:04:23 +0530

    add the sessionwrapper.js for auth

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,16 @@
+"use client"
 import React from 'react'
+import { useSession, signIn, signOut } from "next-auth/react"
 import Link from 'next/link'
 
 const Navbar = () => {
+    const { data: session } = useSession()
+    if(session) {
+        return <>
+          Signed in as {session.user.email} <br/>
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      }
     return (
         <nav className='bg-black shadow-lg text-white flex justify-between items-center px-4 h-16'>
             <div className="logo font-bold text-lg flex justify-center items-center">
