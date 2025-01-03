a0d4d5742c5e22d80616bf1ac69d0c8048b74b6d
tushar626346 <tushar626346@gmail.com>
2025-01-03 22:54:18 +0530

    add logout and dashboard on navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -23,6 +23,8 @@ const Navbar = () => {
                 <li>Login</li>
             </ul> */}
             <div>
+            { session && <Link  href={"/dashboard"}> <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 '>Dashboard</button></Link>}
+            { session &&  <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 ' onClick={()=>{signOut()}}>Logout</button>}
             <Link href={"/login"}>
           <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 '>Login</button></Link>
       </div>
