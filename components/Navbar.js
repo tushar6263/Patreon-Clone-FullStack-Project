74d0135818de6b1b826f14046c9cdbcf9881a67b
tushar626346 <tushar626346@gmail.com>
2025-01-05 13:50:55 +0530

    add UI on the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,19 @@
 "use client"
-import React from 'react'
+import React ,{useState} from 'react'
 import { useSession, signIn, signOut } from "next-auth/react"
 import Link from 'next/link'
 
 const Navbar = () => {
     const { data: session } = useSession()
-    if(session) {
-        return <>
-          Signed in as {session.user.email} <br/>
-          <button onClick={() => signOut()}>Sign out</button>
-        </>
-      }
+    const [showdropdown, setShowdropdown] = useState(false)
+    // if(session) {
+    //     return <>
+    //       Signed in as {session.user.email} <br/>
+    //       <button onClick={() => signOut()}>Sign out</button>
+    //     </>
+      // }
     return (
-        <nav className='bg-black shadow-lg text-white flex justify-between items-center px-4 h-16'>
+        <nav className='bg-neutral-950 shadow-lg text-white flex justify-between items-center px-4 h-16'>
             <div className="logo font-bold text-lg flex justify-center items-center">
                 <img src="tea.gif" width={44} alt="" /><span>GetMeaChai!</span></div>
             {/* <ul className="flex justify-between gap-4">
@@ -23,10 +24,33 @@ const Navbar = () => {
                 <li>Login</li>
             </ul> */}
             <div>
-            { session && <Link  href={"/dashboard"}> <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 '>Dashboard</button></Link>}
-            { session &&  <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 ' onClick={()=>{signOut()}}>Logout</button>}
-            <Link href={"/login"}>
-          <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 '>Login</button></Link>
+            {session && <>
+          <button onClick={() => setShowdropdown(!showdropdown)} onBlur={() => {
+            setTimeout(() => {
+              setShowdropdown(false)
+            }, 100);
+          }} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-gray-900 mx-2 bg-white   font-medium rounded-lg text-sm px-2 py-2.5 text-center inline-flex items-center dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">Account<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+            </svg>
+          </button>
+
+          <div id="dropdown" className={`z-10 ${showdropdown ? "" : "hidden"} absolute right-[18px] top-15 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
+            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+              <li>
+                <Link href="/dashboard" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</Link>
+              </li>
+              <li>
+                <Link href={`/${session.user.name}`} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Your Page</Link>
+              </li>
+              <li>
+                <Link onClick={() => signOut()} href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</Link>
+              </li>
+            </ul>
+          </div></>
+        }
+            { session &&  <button className='text-gray-900 bg-white border border-gray-300 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700' onClick={()=>{signOut()}}>Logout</button>}
+           {!session && <Link href={"/login"}>
+          <button className='text-gray-900 bg-white border border-gray-300 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'>Login</button></Link>}
       </div>
             
         </nav>
