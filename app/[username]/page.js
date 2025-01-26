2faf13f9074cc5261e0a92308663d2f9b934a2df
tushar626346 <tushar626346@gmail.com>
2025-01-26 09:45:22 +0530

    add the text in username>page.js

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -5,12 +5,21 @@ const Username = ({params}) => {
     <>
     <div className='cover w-full relative bg-red-50 '>
       <img className='object-cover w-full h-[350]' src="https://c10.patreonusercontent.com/4/patreon-media/p/campaign/971275/e81e9167015c466486e62f3ccbd2e912/eyJ3Ijo5NjAsIndlIjoxfQ%3D%3D/1.jpg?token-time=1738627200&token-hash=2kN9IWDGCf-CmfrPq7H6urHLB570fmkS7KWkqY7CCL4%3D" alt="" />
-      <div className=' -bottom-10 right-[48%] absolute '>
+      <div className=' -bottom-10 right-[47%] absolute '>
         <img width={80} height={80} className=' border-white border-2 rounded-full' src="https://c10.patreonusercontent.com/4/patreon-media/p/campaign/971275/2406ebe9b7cf44fe9059249e573f8aa7/eyJoIjoxMDgwLCJ3IjoxMDgwfQ%3D%3D/1.jpg?token-time=1739145600&token-hash=YJJ4kgJqbHW5N5R3zR3ooHtHchb3yEsi7qIV3g6j3dM%3D" alt="" />
       </div>
     </div>
-    <div className=" flex justify-center items-center my-12">
+    <div className=" flex justify-center items-center my-12 flex-col gap-2">
+      <div className="font-bold text-lg">
+
     @{params.username}
+      </div>
+      <div className='text-white opacity-60'>
+      creating documentaries, research-based video content etc..
+      </div>
+      <div className='text-white opacity-60'>
+      928 members . 125 posts . $15,324/release
+      </div>
     </div>
   </>
   )
